4b0495c13e138092d393d727af338e1d61ddad33
Nebiyu <Nebiyuhiyabu@gmail.com>
2023-07-25 18:59:37 +0000

    default parameters

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,4 +1,4 @@
-export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) { explansion1989, explansion2019 = typeof explansion1989 === undefined ? 89, typeof expansion2019 === undefined ? 19; return initialNumber +expansion1989 + expansion2019; }
+export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) { explansion1989, explansion2019 = typeof explansion1989 === undefined ? 89, typeof expansion2019 === undefined ? 19; return initialNumber + expansion1989 + expansion2019; }
 /*
 if (expansion1989 === undefined) {
     expansion1989 = 89;
