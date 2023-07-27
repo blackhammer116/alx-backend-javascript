a9e53401173a336979b53c0d2bef3726b74445ae
Nebiyu <nebiyuhiyabu@gmail.com>
2023-07-27 22:11:43 +0000

    EVCar

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,19 @@
+import Car from './10-car';
+
+const clonedCarSymbol = Symbol('cloneCar');
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  [clonedCarSymbol]() {
+    const { _brand, _motor, _color} = this;
+    return new Car(_brand, _motor, _color);
+  }
+
+  cloneCar() {
+    return this[clonedCarSymbol]();
+  }
+}
