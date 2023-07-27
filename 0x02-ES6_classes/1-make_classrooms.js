f5538791af6a161c9277e332cccceec0972a8ff8
Nebiyu <nebiyuhiyabu@gmail.com>
2023-07-27 14:57:43 +0000

    making a classroom

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,10 @@
 import ClassRoom from './0-classroom.js';
 
 function initializeRooms() {
-  const arr = {
+  const arr = [
     new ClassRoom(19),
     new ClassRoom(20),
     new ClassRoom(34)
-  };
+  ];
   return arr;
 }
