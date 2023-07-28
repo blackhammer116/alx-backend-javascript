40d7feff85e185de12117bb2582356bfe557b2dd
Nebiyu <nebiyuhiyabu@gmail.com>
2023-07-28 10:11:27 +0000

    creating an employees object

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,10 @@
 export default function appendToEachArrayValue(array, appendString) {
+  let i = 0;
   for (let idx of array) {
-    let value = idx;
+    const value = idx;
     idx = appendString + value;
+    array[i] = idx;
+    i += 1;
   }
 
   return array;
