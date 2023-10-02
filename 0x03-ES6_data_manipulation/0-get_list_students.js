250d778d98420520d4e7bb6b969de25cb4e1153f
Nebiyu <nebiyuhiyabu@gmail.com>
2023-10-03 00:19:35 +0300

    getting list of students

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,10 @@
+function getListStudents() {
+	ob1 = {id: 1, firstName: 'Guillaume', location:  'San Francisco'};
+	ob2 = {id: 2, firstName: 'James', location:  'Columbia'};
+	ob3 = {id: 5, firstName: 'Serena', location:  'San Francisco'};
+
+	arr = [obj1, obj2, obj3];
+
+	return arr;
+}
+module.exports = getListStudents();
