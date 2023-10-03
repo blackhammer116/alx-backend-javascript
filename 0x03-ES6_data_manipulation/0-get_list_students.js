6588db0235bfdb2abc180327a3a065998359e68a
Nebiyu <nebiyuhiyabu@gmail.com>
2023-10-03 03:04:31 +0000

    getting the list of students

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,17 @@
-function getListStudents() {
-	ob1 = {id: 1, firstName: 'Guillaume', location:  'San Francisco'};
-	ob2 = {id: 2, firstName: 'James', location:  'Columbia'};
-	ob3 = {id: 5, firstName: 'Serena', location:  'San Francisco'};
+module.exports = function getListStudents() {
 
-	arr = [obj1, obj2, obj3];
+	const obj = new Set();
+
+	obj.add({id: 1, firstname: "Guillaume", location: "San Francisco"});
+	obj.add({id: 2, firstname: "James", location: "Columbia"});
+	obj.add({id: 5, firstname: "Serena", location: "San Francisco"});
+
+	const arr = [];
+	const obj2 = obj.values();
+	
+	arr.push(obj2.next().value);
+	arr.push(obj2.next().value);
+	arr.push(obj2.next().value);
 
 	return arr;
 }
-module.exports = getListStudents();
