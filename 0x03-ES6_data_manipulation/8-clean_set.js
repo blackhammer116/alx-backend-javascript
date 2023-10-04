b6ebe2d0566b005a2477d0c48048e4c2065789ac
Nebiyu <nebiyuhiyabu@gmail.com>
2023-10-04 02:21:57 +0000

    cleaning up

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,8 @@
 module.exports = function cleanSet(set, startString) {
   let result = '';
-
+  if (startString.length === 0) {
+    return result;
+  }
   for (const value of set) {
     if (value.startsWith(startString)) {
       const cleanedValue = value.slice(startString.length);
